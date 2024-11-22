37016e2436135e89eb1e90fb30eb869ad6dcbf85
Darioooo <darionietosastre@gmail.com>
2024-11-22 10:40:07 +0100

    se añade un array de twitts y se introduce en el html con foreach

--- 7.-PHP_Twitter_Require/index.php
+++ 7.-PHP_Twitter_Require/index.php
@@ -1,10 +1,33 @@
 <?php 
 $userName = '@Dario';
-$comentario = 'La Gear5 de Luffy es el dios sol Nika, o Joyboy';
+$texto = 'La Gear5 de Luffy es el dios sol Nika, o Joyboy';
 $autor = 'DarioTwitter';
 
+$twitts = [
+    [
+        "texto" => $texto,
+        "autor" => $autor,
+        "userName" => $userName
+    ],
+    [
+        "texto" => "Este es el texto del segundo twitt",
+        "autor" => "Jose Luis",
+        "userName" => "@joselu"
+    ],
+    [
+        "texto" => "Este es el texto del tercer twitt",
+        "autor" => "Juan",
+        "userName" => "@juanillo"
+    ],
+    [
+        "texto" => "Este es el texto del cuarto twitt",
+        "autor" => "Julia",
+        "userName" => "@vjuliaTwitter"
+    ]
+];
 
-require_once "./index.html";
+// El orden donde se coloca el require importa, todo lo que quedamos añadir al archivo tiene que estar antes de la llamada a éste.
+require_once "./index.html"; 
 
 
 ?>
